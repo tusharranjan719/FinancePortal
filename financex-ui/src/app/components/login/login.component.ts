75539acdbb381208272ec9dbd71fd9a208ba70fb
Tushar Ranjan <tushar@Tushars-MacBook.local>
2022-02-02 19:10:42 -0500

    Added assets, css, and images

--- financex-ui/src/app/components/login/login.component.ts
+++ financex-ui/src/app/components/login/login.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
-
 @Component({
   selector: 'financex-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-
   constructor(private fb: FormBuilder) { }
-
   ngOnInit(): void {
   }
-
   loginForm = this.fb.group({
     email: ['', Validators.required, Validators.pattern(this.emailRegx)],
     password: ['', Validators.required],
   });
-
+  signupForm = this.fb.group({
+    email: ['', Validators.required, Validators.pattern(this.emailRegx)],
+    password: ['', Validators.required],
+  });
+  submit(){
+  }
 }
