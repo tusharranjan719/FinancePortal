c78cd8fa2968893b06e72b8b320fba55cfdb39ec
Mehul Jhaver <mehuljhaver@gmail.com>
2022-03-31 19:21:28 -0400

    Added user info to signup page

---
+++ homepage/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'footer', component: FooterComponent },
+  // { path: 'sidebar', component: SidebarComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
