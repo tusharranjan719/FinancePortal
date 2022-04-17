accafdf0d4da5e4a064e2dca6baf0263f8da66e4
Tushar Ranjan <tusharranjan719@live.com>
2022-04-17 00:18:30 -0400

    Added fetch expense

--- financex-Backend/dashboard/backend/data/data.go
+++ financex-Backend/dashboard/backend/data/data.go
@@ -200,6 +200,12 @@ func SplitBill() (billSplits []BillSplit, err error) {
 	return
 }
 
+func SearchExpenseByUuid(name string) (expense Expense, err error) {
+	err = Db.QueryRow("SELECT e.id, e.uuid, e.name, e.amount, e.billsplit_id, p.name, e.created_at FROM expense e INNER JOIN participant p ON e.participant_id = p.id where e.uuid = $1", name).
+		Scan(&expense.Id, &expense.Uuid, &expense.Name, &expense.Amount, &expense.BillSplitID, &expense.PayerName, &expense.CreatedAt)
+	return
+}
+
 // ParticipantByID gets an Participant record in the DB by its id (unique)
 func ParticipantByID(id int) (participant Participant, err error) {
 	err = Db.QueryRow("SELECT id, uuid, name, billsplit_id, created_at FROM participant WHERE id = $1", id).
